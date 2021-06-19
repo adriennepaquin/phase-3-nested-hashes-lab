e3f324a2f30d133d9a2acc63f4415bec181f5a59
Adrienne <adriennepaquin@gmail.com>
2021-06-19 03:19:15 -0400

    done

--- nested.rb
+++ nested.rb
@@ -16,11 +16,13 @@ def hopper
       }
     }
 
+  programmer_hash[:grace_hopper]
+
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
+
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -37,6 +39,7 @@ def alan_kay_is_known_for
       }
     }
 
+    programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+  
+  programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -81,6 +86,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
+  programmer_hash
 
 end
 
@@ -104,6 +114,8 @@ def changing_alan
       }
     }
 
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +137,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+  
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
 end
